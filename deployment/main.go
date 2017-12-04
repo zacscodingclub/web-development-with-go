fb5c9a3fe03e7ac5e55b4bba9a411234a82607ec
zacscodingclub <zbaston@gmail.com>
2017-12-04 11:19:39 -0800

    update main to mimic example

--- deployment/main.go
+++ deployment/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 )
 
-func init() {
+func main() {
 	http.Handle("/", http.FileServer(http.Dir(".")))
+	http.HandleFunc("/_ah/health", healthCheckHandler)
+	log.Print("Listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "ok")
 }
